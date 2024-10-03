8b866d00693b3916b8ac1cf50f9414bfa467181a
Soudeh <babaeizadeh.s@gmail.com>
2024-10-03 22:18:52 +0330

    update

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,6 +1,15 @@
 import React, { useReducer } from "react";
 import { Button } from "@/components/ui/button";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 interface State {
   count: number;
   error: string | null;
@@ -47,17 +56,27 @@ const Demo = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col gap-5">
-        <div className="text-xl font-bold">Count: {state.count}</div>
-        {state.error && <div className="text-red-500">{state.error}</div>}
+      <Card className="w-[350px] p-10">
+        <CardHeader>
+          {" "}
+          <CardTitle className="mx-auto">Counter Mini App</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-5">
+            <div className="text-xl font-bold mx-auto">
+              Count: {state.count}
+            </div>
+            {state.error && <div className="text-red-500">{state.error}</div>}
 
-        <Button onClick={() => dispatch({ type: "increment" })}>
-          Increment
-        </Button>
-        <Button onClick={() => dispatch({ type: "decrement" })}>
-          Decrement
-        </Button>
-      </div>
+            <Button onClick={() => dispatch({ type: "increment" })}>
+              Increment
+            </Button>
+            <Button onClick={() => dispatch({ type: "decrement" })}>
+              Decrement
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
