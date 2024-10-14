b299cfdf9d995eaf5bed322b7db3e267b31da03d
Soudeh <babaeizadeh.s@gmail.com>
2024-10-14 12:34:32 +0330

    improvement

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import { useReducer } from "react";
 import { Button } from "@/components/ui/button";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
